804049302214f9f37d5c59141d1f8775a66bebfd
lokha <aleksey.kovalchuk98@gmail.com>
2021-09-18 19:04:13 +0300

    Убрал удаление чанка с диска при превышении лимита

--- src/main/java/ua/lokha/tileentityradiuslimiter/ChunkChecker.java
+++ src/main/java/ua/lokha/tileentityradiuslimiter/ChunkChecker.java
@@ -1,21 +1,16 @@
 package ua.lokha.tileentityradiuslimiter;
 
-import lombok.SneakyThrows;
-import net.minecraft.server.v1_12_R1.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraft.server.v1_12_R1.Chunk;
+import net.minecraft.server.v1_12_R1.TileEntity;
+import net.minecraft.server.v1_12_R1.WorldServer;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.nio.file.Files;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ChunkChecker implements Runnable {
 
@@ -87,8 +82,6 @@ public class ChunkChecker implements Runnable {
                         try {
                             handle.getChunkProviderServer().unloadQueue.remove(chunk.chunkKey);
                             handle.getChunkProviderServer().chunks.remove(chunk.chunkKey);
-                            RegionFile regionFile = RegionFileCache.a(world.getWorldFolder(), chunk.locX, chunk.locZ);
-                            deleteChunk(regionFile, chunk.locX & 31, chunk.locZ & 31);
                         } catch (Exception e) {
                             Main.getInstance().getLogger().severe("Ошибка при удалении чанка " +
                                     chunk.locX + " " + chunk.locZ + " в мире " + world.getName());
@@ -114,49 +107,6 @@ public class ChunkChecker implements Runnable {
 
         public Counter(int limit) {
             this.limit = limit;
-        }
-    }
-
-    private static Method setOffset;
-    private static Method setTimestamp;
-    private static Field fileField;
-
-    static {
-        try {
-            fileField = RegionFile.class.getDeclaredField("b");
-            fileField.setAccessible(true);
-
-            setOffset = RegionFile.class.getDeclaredMethod("a", int.class, int.class, int.class);
-            setOffset.setAccessible(true);
-
-            setTimestamp = RegionFile.class.getDeclaredMethod("b", int.class, int.class, int.class);
-            setTimestamp.setAccessible(true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @SneakyThrows
-    public static void deleteChunk(RegionFile regionFile, int x, int z) {
-        backup((File) fileField.get(regionFile));
-        setOffset.invoke(regionFile, x, z, 0);
-        setTimestamp.invoke(regionFile, x, z, 0);
-    }
-
-    private static void backup(File file) {
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date today = new Date();
-        File corrupt = new File(file.getParentFile(), file.getName() + "." + formatter.format(today) + ".corrupt");
-        if (!corrupt.exists()) {
-            Logger logger = LogManager.getLogger();
-            logger.error("Region file " + file.getAbsolutePath() + " was corrupt. Backing up to " + corrupt.getAbsolutePath() + " and repairing");
-
-            try {
-                Files.copy(file.toPath(), corrupt.toPath());
-            } catch (IOException var7) {
-                logger.error("Error backing up corrupt file" + file.getAbsolutePath(), var7);
-            }
-
         }
     }
 }
